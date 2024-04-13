e70eb1680404818e2063665a8b9ddba031a29390
fdoargolo <felipeargolo75@gmail.com>
2024-04-13 13:01:35 -0300

    cleaning code

--- script.js
+++ script.js
@@ -2,32 +2,27 @@ const button = document.querySelector('#check-btn');
 const result = document.querySelector('#result');
 const textInput = document.querySelector('.text');
 
-function reverseString(text){
-    text = text.split("").reverse().join("");
-    return text; 
-}
-function removeSpace(text){
-    text = text.split(" ").join("");
-    return text; 
-}
+//arrow functions to do the work
+const reverseString = (text) => text = text.split("").reverse().join(""); 
+const removeSpace = (text) => text.split(" ").join("");
 
 
 button.addEventListener("click", function(){
     
     let text = textInput.value;
-    let firstInput = text;
+    let originalText = text;
     text = text.trim(); 
+
     text = removeSpace(text);
-    
     reversedText = reverseString(text);
 
     if(text == ""){
         alert("Please input a value");
     }else if(reversedText == text){
         result.style.color = 'green'; //coloring a little
-        result.innerHTML = firstInput + " is a palindrome.";
+        result.innerHTML = originalText + " is a palindrome.";
     } else{
         result.style.color = 'red';
-        result.innerHTML = firstInput + " is not a palindrome.";
+        result.innerHTML = originalText + " is not a palindrome.";
     }
 });
